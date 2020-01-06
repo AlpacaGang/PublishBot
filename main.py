fe71fcc2ec15e2e21f51399e401da43c4898cb5c
Ivan1874 <ivan51673@gmail.com>
2020-01-06 17:22:27 +0300

    Before build

---
+++ main.py
@@ -0,0 +1,24 @@
+import glob
+import os
+import sys
+
+from telegram import Bot, ParseMode
+
+bot = Bot(os.environ.get('token'))
+if '--before' in sys.argv:
+    bot.send_message(chat_id=-1001115967921,
+                     text=f'⚙️ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
+                          f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> started...',
+                     parse_mode=ParseMode.HTML)
+elif '--after' in sys.argv:
+    if os.environ.get('TRAVIS_TEST_RESULT') == 0:
+        bot.send_message(chat_id=-1001115967921,
+                         text=f'✅ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
+                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> succeed!',
+                         parse_mode=ParseMode.HTML)
+        bot.send_document(chat_id=-1001115967921, document=open(glob.glob('HMP-kernel-*.zip')[0], 'rb'))
+    else:
+        bot.send_message(chat_id=-1001115967921,
+                         text=f'❌ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
+                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> failed!',
+                         parse_mode=ParseMode.HTML)
