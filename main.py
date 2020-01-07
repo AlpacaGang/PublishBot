0dba6f96aa0d3855c368a3eb0c01de3d6e158211
Ivan1874 <ivan51673@gmail.com>
2020-01-07 10:05:58 +0300

    added build time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import time
 
 from telegram import Bot, ParseMode
 
@@ -12,15 +13,25 @@ if '--before' in sys.argv:
                      text=f'⚙️ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
                           f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> started...',
                      parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+    open('~/time', 'w').write(str(round(time.time())))
 elif '--after' in sys.argv:
+    build_time = time.time() - int(open('~/time', 'r').read())
+    m, s = divmod(build_time, 60)
+    h, m = divmod(m, 60)
+    build_time_str = f'{s} sec'
+    if m > 0:
+        build_time_str = f'{m} min' + build_time_str
+        if h > 0:
+            build_time_str = f'{h} hrs ' + build_time_str
+
     if os.environ.get('TRAVIS_TEST_RESULT') == 0:
         bot.send_message(chat_id=chat_id,
                          text=f'✅ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
-                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> succeed!',
+                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> succeed in a {build_time_str}!',
                          parse_mode=ParseMode.HTML, disable_web_page_preview=True)
         bot.send_document(chat_id=chat_id, document=open(glob.glob(product_filename)[0], 'rb'))
     else:
         bot.send_message(chat_id=chat_id,
                          text=f'❌ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
-                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> failed!',
+                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> failed in a {build_time_str}!',
                          parse_mode=ParseMode.HTML, disable_web_page_preview=True)
