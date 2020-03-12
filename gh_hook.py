84ab0326d09df6f31996302024abf14dbe8fdd6a
Ivan1874 <ivan51673@gmail.com>
2020-03-12 18:45:39 +0300

    Fixed long pushes

--- gh_hook.py
+++ gh_hook.py
@@ -18,13 +18,17 @@ def trigger(user, repo, chat_id):
     chat_id = int(chat_id)
     data = json.loads(request.data)
     commits = []
+    len_now = 0
     for commit in data['commits']:
-        commits.append(
-            f'<a href="{escape(commit["url"])}">{escape(commit["id"][:7])}</a>: <code>{escape(commit["message"])}</code> by {escape(commit["author"]["name"])}')
+        commit = f'<a href="{escape(commit["url"])}">{escape(commit["id"][:7])}</a>: <code>{escape(commit["message"])}</code> by {escape(commit["author"]["name"])}'
+        len_now += len(commit)
+        if len_now > 3900:
+            break
+        commits.append(commit)
     bot.send_message(chat_id=chat_id,
-                     text=f'🔨 {len(commits)} new {"commit" if len(commits) == 1 else "commits"} '
+                     text=f'🔨 {len(data["commits"])} new {"commit" if len(data["commits"]) == 1 else "commits"} '
                           f'to <b>{escape(repo)}:{escape(data["ref"].split("/")[-1])}</b>:\n\n' +
-                          "\n".join(commits)[:3900],
+                          "\n".join(commits),
                      parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     return 'OK'
 
