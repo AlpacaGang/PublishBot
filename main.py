62a53fc1b12be64661aab7fe8b2a5dcb3ba36d01
FedorShatokhin2005 <groza01i@gmail.com>
2020-03-23 20:10:24 +0500

    Checkout to local builds
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ chat_id = int(os.environ.get('chat_id'))
 product_filename = os.environ.get('product_filename')
 if '--before' in sys.argv:
     bot.send_message(chat_id=chat_id,
-                     text=f'⚙️ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
-                          f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> started...',
+                     text=f'⚙️ Local build for platina started...',
                      parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-    open('/home/travis/time', 'w').write(str(round(time.time())))
+    open('/home/fedshat/time', 'w').write(str(round(time.time())))
 elif '--after' in sys.argv:
-    build_time = int(time.time()) - int(open('/home/travis/time', 'r').read())
+    build_time = int(time.time()) - int(open('/home/fedshat/time', 'r').read())
     m, s = divmod(build_time, 60)
     h, m = divmod(m, 60)
     build_time_str = f'{s} sec'
@@ -26,14 +25,12 @@ elif '--after' in sys.argv:
 
     files_found = glob.glob(product_filename)
 
-    if (os.environ.get('TRAVIS_TEST_RESULT') == '0') and (len(files_found) > 0):
+    if len(files_found) > 0:
         bot.send_message(chat_id=chat_id,
-                         text=f'✅ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
-                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> succeed in a {build_time_str}!',
+                         text=f'✅ Local build for platina succeed in a {build_time_str}!',
                          parse_mode=ParseMode.HTML, disable_web_page_preview=True)
         bot.send_document(chat_id=chat_id, document=open(files_found[0], 'rb'))
     else:
         bot.send_message(chat_id=chat_id,
-                         text=f'❌ Build <a href="{os.environ.get("TRAVIS_BUILD_WEB_URL")}">'
-                              f'#{os.environ.get("TRAVIS_BUILD_NUMBER")}</a> failed in a {build_time_str}!',
+                         text=f'❌ Local build for platina failed in a {build_time_str}!',
                          parse_mode=ParseMode.HTML, disable_web_page_preview=True)
