e33d88b094e07cedca996a0c94897128b65894b0
FedorShatokhin2005 <groza01i@gmail.com>
2020-06-23 16:56:48 +0500

    kernel: Update  miui ak3 tree too
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- kernel.py
+++ kernel.py
@@ -48,6 +48,7 @@ def update_tree(p, b):
 
 update_tree('.', 'staging')
 update_tree('../AK3', 'master')
+update_tree('../AK3MIUI', 'miui')
 update_tree('../tools/arm64-gcc', 'master')
 
 commit_msg = escape_markdown(repo.active_branch.commit.message.split("\n")[0], version=2)
