bea47b5f1e0ca61161609583fdce56ce5f653b5e
Anton Egorov <a12286@yandex.com>
2020-07-03 10:25:29 +0300

    use html )0))

--- drone.py
+++ drone.py
@@ -3,8 +3,8 @@ import argparse
 import os
 
 
-def esc(text, entity_type=None):
-    return telegram.utils.helpers.escape_markdown(text, 2, entity_type)
+def esc(text, quote=False):
+    return telegram.utils.helpers.escape(text, quote)
 
 
 CHAT_ID = os.getenv("CHAT_ID")
@@ -24,16 +24,16 @@ bot = telegram.Bot(token=os.getenv("BOT_TOKEN"))
 args = parse_args()
 
 BUILD_NUMBER = os.getenv("DRONE_BUILD_NUMBER", "<unknown>")
-COMMIT = f'[{esc(os.getenv("DRONE_COMMIT_LINK"))}]'\
-               f'({esc(os.getenv("DRONE_COMMIT_SHA", "0000000")[:7])})'\
+COMMIT = f'<a href="{esc(os.getenv("DRONE_COMMIT_LINK"))}">'\
+               f'{esc(os.getenv("DRONE_COMMIT_SHA", "0000000")[:7])}</a>'\
                f' by {esc(os.getenv("DRONE_COMMIT_AUTHOR", "anonymous"))}'
-REPO = f'[{esc(os.getenv("DRONE_REPO_LINK"))}]({esc(os.getenv("DRONE_REPO"))})'
+REPO = f'<a href="{esc(os.getenv("DRONE_REPO_LINK"))}">{esc(os.getenv("DRONE_REPO"))}</a>'
 if args.ok:
     print(f'✅ Pipeline {esc(BUILD_NUMBER)} for {REPO} (commit {COMMIT}) succeed!')
     bot.send_message(chat_id=CHAT_ID, text=f'✅ Pipeline {esc(BUILD_NUMBER)} for '
                                            f'{REPO} (commit {COMMIT}) succeed!',
-                     parse_mode='MarkdownV2')
+                     parse_mode='HTML')
 else:
     bot.send_message(chat_id=CHAT_ID, text=f'❌ Pipeline {esc(BUILD_NUMBER)} for '
                                            f'{REPO} (commit {COMMIT}) failed!',
-                     parse_mode='MarkdownV2')
+                     parse_mode='HTML')
