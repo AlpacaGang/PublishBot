79a36906775e2af8612fac06d5d4735489f78462
FedorShatokhin2005 <groza01i@gmail.com>
2020-07-06 01:33:51 +0300

    kernel: Update for rebase
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- kernel.py
+++ kernel.py
@@ -18,13 +18,13 @@ start_time = time()
 TIMESTAMP = datetime.now()
 
 CHAT_ID = -1001115967921
-FILENAME = f'../AlpacaKernel-r18-{TIMESTAMP.strftime("%Y%m%d-%H%M")}-unsigned.zip'
+FILENAME = f'../AlpacaKernel-r19-{TIMESTAMP.strftime("%Y%m%d-%H%M")}-unsigned.zip'
 COMPILER_STRING = 'GCC 10.x'
-KERNEL_VERSION = 'Alpaca, r18, LTO'
+KERNEL_VERSION = 'Alpaca, r19, LTO'
 DEVICE = 'platina'
 DEFCONFIG = 'platina_defconfig'
 CROSS_COMPILE = expanduser('~') + '/build/tools/arm64-gcc/bin/aarch64-elf-'
-REPO = 'AlpacaGang/kernel_xiaomi_platina'
+REPO = 'FedorShatokhin2005/msm-4.4'
 NPROC = multiprocessing.cpu_count()
 
 X508_PATH = expanduser('~') + '/certificate.pem'
@@ -47,7 +47,7 @@ update_tree('.', 'staging')
 update_tree('../AK3', 'master')
 update_tree('../tools/arm64-gcc', 'master')
 
-SIGNED_FILENAME = f'../AlpacaKernel-r18-{TIMESTAMP.strftime("%Y%m%d-%H%M")}-{repo.active_branch.commit.hexsha[:8]}.zip'
+SIGNED_FILENAME = f'../AlpacaKernel-r19-{TIMESTAMP.strftime("%Y%m%d-%H%M")}-{repo.active_branch.commit.hexsha[:8]}.zip'
 
 commit_msg = escape_markdown(repo.active_branch.commit.message.split("\n")[0], version=2)
 commit = f'`{repo.active_branch.name}:' \
