6a0ddd637778d83745a84bbfff70c56ed77a05bf
FedorShatokhin2005 <groza01i@gmail.com>
2020-07-14 10:54:29 +0300

    Don't replace localversion
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- kernel.py
+++ kernel.py
@@ -64,10 +64,6 @@ if os.path.isfile('.config'):
     os.remove('.config')
 print('========== Making defconfig ==========')
 os.system(f'make O=out ARCH=arm64 {DEFCONFIG}')
-with open('out/.config', 'r') as f:
-    data = f.read()
-with open('out/.config', 'w') as f:
-    f.write(data.replace('Alpaca', f'Alpaca-{repo.active_branch.commit.hexsha[:8]}-{repo.active_branch.name}'))
 print('========== Building kernel ==========')
 if not os.system(f'make -j{NPROC} O=out ARCH=arm64 CROSS_COMPILE={CROSS_COMPILE}'):
     print('========== Build succeed ==========')
