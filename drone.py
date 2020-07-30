f8dd4d9431848a767ca4f932bd8b3e94d6d54a12
FedorShatokhin2005 <groza01i@gmail.com>
2020-07-30 16:53:07 +0500

    drone: Add '#' before BUILD_LINK
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- drone.py
+++ drone.py
@@ -18,10 +18,10 @@ bot = stg.TelegramAPI(token=os.getenv("BOT_TOKEN"))
 
 DRONE_LINK = 'https://drone.vanutp.dev/'
 BUILD_NUMBER = esc(os.getenv("DRONE_BUILD_NUMBER"))
-BUILD_LINK = f'<a href="{esc(DRONE_LINK)}{esc(os.getenv("DRONE_REPO"))}/{BUILD_NUMBER}">{BUILD_NUMBER}</a>'
+BUILD_LINK = f'<a href="{esc(DRONE_LINK)}{esc(os.getenv("DRONE_REPO"))}/{BUILD_NUMBER}">#{BUILD_NUMBER}</a>'
 COMMIT = f'<a href="{esc(os.getenv("DRONE_COMMIT_LINK"))}">'\
-               f'{esc(os.getenv("DRONE_COMMIT_SHA")[:7])}</a>'\
-               f' by {esc(os.getenv("DRONE_COMMIT_AUTHOR_NAME"))}'
+    f'{esc(os.getenv("DRONE_COMMIT_SHA")[:7])}</a>'\
+    f' by {esc(os.getenv("DRONE_COMMIT_AUTHOR_NAME"))}'
 REPO = f'<a href="{esc(os.getenv("DRONE_REPO_LINK"))}">{esc(os.getenv("DRONE_REPO"))}</a>'
 
 if '--start' in sys.argv:
