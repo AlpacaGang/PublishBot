16eefaca868957701c93d1ac0a2d581ec09a7f7e
FedorShatokhin2005 <groza01i@gmail.com>
2020-08-10 19:59:44 +0500

    Upload with telethon's TelegramClient
    Signed-off-by: FedorShatokhin2005 <groza01i@gmail.com>

--- lavender.py
+++ lavender.py
@@ -1,15 +1,17 @@
 import os
 
-from telegram import Bot
+from telethon.sync import TelegramClient
 
 os.environ['KBUILD_BUILD_USER'] = 'fedshat'
 os.environ['KBUILD_BUILD_HOST'] = 'fedshatci'
 os.environ['TZ'] = 'Europe/Moscow'
 os.environ['KERNEL_USE_CCACHE'] = '1'
 CHAT_ID = -1001222275544
-bot = Bot(os.environ.get('TOKEN'))
+
+bot = TelegramClient('bot', int(os.environ['API_ID']), os.environ['API_HASH']).start(
+    bot_token=os.environ['TOKEN'])
 
 if not os.system('bash -c "source build/envsetup.sh; lunch lineage_lavender-userdebug; mka bootimage"'):
     print('========== Build succeed ==========')
     FILENAME = 'out/target/product/lavender/boot.img'
-    bot.send_document(chat_id=CHAT_ID, document=open(FILENAME, 'rb'))
+    bot.send_file(CHAT_ID, FILENAME)
