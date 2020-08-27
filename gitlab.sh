40306724bfea3c43384dce68e0639286eba62a01
fox <ivan51673@gmail.com>
2020-08-27 10:27:29 +0300

    fix gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -21,7 +21,7 @@ INSTANCE_URL="https://git.vanutp.dev"
 REPO_LINK="$INSTANCE_URL/$CI_PROJECT_PATH"
 COMMIT_LINK="$REPO_LINK/-/commit/$CI_COMMIT_SHA"
 BUILD_LINK="<a href=\"$(escape_html $REPO_LINK/-/jobs/$CI_PIPELINE_ID)\">#$CI_PIPELINE_ID</a>"
-COMMIT="<a href=\"$(escape_html $COMMIT_LINK)\">${DRONE_COMMIT_SHA::7}</a>"
+COMMIT="<a href=\"$(escape_html $COMMIT_LINK)\">${CI_COMMIT_SHA::7}</a>"
 REPO="<a href=\"$(escape_html $REPO_LINK)\">$(escape_html $CI_PROJECT_PATH)</a>"
 
 if [[ $1 == "--start" ]]; then
